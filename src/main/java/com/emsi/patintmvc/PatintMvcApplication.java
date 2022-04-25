9cff06665583a8dcced0689176bb71bc314098a8
badr6 <Badr65benfadel@gmail.com>
2022-04-25 10:28:06 +0000

    CLASS PA

--- src/main/java/com/emsi/patintmvc/PatintMvcApplication.java
+++ src/main/java/com/emsi/patintmvc/PatintMvcApplication.java
@@ -11,7 +11,7 @@ import java.util.Date;
 
 @SpringBootApplication
 public class PatintMvcApplication {
-
+    ///
     public static void main(String[] args) {
         SpringApplication.run(PatintMvcApplication.class, args);
     }
